22fd38f3b0878327b67519742be1dc8072543637
yuberee <59583743+yuberee@users.noreply.github.com>
2022-08-24 00:17:04 +0200

    Music!

---
+++ code/Game/Game.Music.cs
@@ -0,0 +1,32 @@
+﻿namespace Stud;
+
+public partial class Game
+{
+
+	Sound? backgroundMusic;
+
+	[Event.Tick.Server]
+	public void LoadMusic()
+	{
+
+		if ( backgroundMusic == null )
+		{
+
+			backgroundMusic = Sound.FromScreen( "goumi-remix" );
+
+		}
+		else
+		{
+
+			if ( backgroundMusic.Value.Finished )
+			{
+
+				backgroundMusic = Sound.FromScreen( "goumi-remix" ); // You cannot stop it.
+
+			}
+
+		}
+
+	}
+
+}
